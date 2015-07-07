a87af5e05de5ea3fabb0948ca2a3def39caad8c4
xpmozong <361131953@qq.com>
2015-07-07 11:07:04 +0800

    index.php修改

--- demos/enterprise/index.php
+++ demos/enterprise/index.php
@@ -2,8 +2,6 @@
 header('Content-Type:text/html;charset=utf-8');
 // error_reporting(0);
 
-define('STATIC_URL', 'http://42.51.161.193:8888/');
-
 require dirname(__FILE__).'/../../wavephp/Wave.php';
 $config = dirname(__FILE__).'/protected/config/main.php';
 
